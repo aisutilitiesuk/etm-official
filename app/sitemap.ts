f52d5552003536df1605a36d490bb4bf1a0e2806
AISUTILITIESUK <aisutilitiesuk@gmail.com>
2026-05-05 14:16:25 +0100

    Add location pages to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -64,5 +64,23 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.9,
     },
+    {
+      url: `${baseUrl}/traffic-management-st-albans`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/traffic-management-london`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/traffic-management-hertfordshire`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
   ];
 }
